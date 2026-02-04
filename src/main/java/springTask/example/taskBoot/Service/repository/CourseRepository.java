21405eb5dd8eef3566f065182b92f06cb5588797
Parveen Mulla <parveenmulla265@gmail.com>
2026-02-04 23:54:18 +0530

    Delete src/main/java/springTask/example/taskBoot/Service directory

--- src/main/java/springTask/example/taskBoot/Service/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package springTask.example.taskBoot.repository;
-
-import springTask.example.taskBoot.entities.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course,Integer> {
-}
